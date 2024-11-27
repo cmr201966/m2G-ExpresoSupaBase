e37c68296e78edf870efeac266b9003043e65d87
Carlos Mora <cmr201966@gmail.com>
2024-11-27 14:33:48 -0500

    en bigslider cuando el anuncio no tiene link

--- src/components/BigSlider/BigSlider.jsx
+++ src/components/BigSlider/BigSlider.jsx
@@ -116,7 +116,11 @@ const BigSlider = (props) => {
                   <div key={i} className="big-slider-item">
                        <img className="img-slider" src={imagenes[i]} alt={nombres[i]} />
                   </div>
-                </a>:""
+                </a>:
+                  <div key={i} className="big-slider-item">
+                      <img className="img-slider" src={imagenes[i]} alt={nombres[i]} />
+                  </div>
+
             ))}
           {imgsFileName?.length && (
             links[0]?.indexOf("https:") === -1 && links[0]?.length!==0 ?
@@ -139,7 +143,10 @@ const BigSlider = (props) => {
             <div key={0} className="big-slider-item">
                      <img className="img-slider" src={imagenes[0]} alt={nombres[0]} />
                 </div>
-              </a>:""
+              </a>:
+                  <div key={0} className="big-slider-item">
+                     <img className="img-slider" src={imagenes[0]} alt={nombres[0]} />
+                  </div>
 
           )}
         </div>
