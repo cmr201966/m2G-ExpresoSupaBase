7ebe738a898ab68d394e18e8ee3933f54a71eec9
Carlos Mora <cmr201966@gmail.com>
2024-11-28 08:51:44 -0500

    quite console.log

--- src/views/Acercade/Acercade.jsx
+++ src/views/Acercade/Acercade.jsx
@@ -39,7 +39,6 @@ const Acercade = () => {
 
   async function init() {
     //let db = await CreaTablaBaseDatos();
-    //console.log(db)
     let resultado = await getJpgFileSB(
       "logo.jpg",
       "./galerias/app_images/destodo/",
