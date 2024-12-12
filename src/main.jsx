d26c0ac47dabf4f7a5ec9776253fdb04446c692e
Carlos Mora <cmr201966@gmail.com>
2024-12-12 16:56:23 -0500

    dfggd

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ import "tippy.js/dist/tippy.css";
 import "./index.css";
 sessionStorage.setItem("sgbd", "SUPABASE");
 //sessionStorage.setItem("sgbd","MYSQL");
-console.log("Otra vez");
+console.log("Otra");
 import "@fontsource/poppins";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
